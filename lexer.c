bc680bfca96241c50a79bc67777eecc4aadbfbb2
Chirag423 <yadavchirag229@gmail.com>
2025-07-03 15:07:43 +0530

    string token

--- lexer.c
+++ lexer.c
@@ -2,6 +2,7 @@
 #include "compiler.h"
 #include "helpers/vector.h"
 #include "helpers/buffer.h"
+#include "assert.h"
 
 #define LEX_GETC_IF(buffer, c, exp)     \
     for (c = peekc(); exp; c = peekc()) \
@@ -92,6 +93,26 @@ struct token* token_make_number()
      return token_make_number_for_value(read_number());
 }
 
+static struct token* token_make_string(char start_delim, char end_delim)
+{
+    struct buffer* buf = buffer_create();
+    assert(nextc() == start_delim);
+    char c = nextc();
+    for (; c != end_delim && c!= EOF; c = nextc())
+    {
+        if (c =='\\')
+        {
+            //handel escape character.
+            continue;
+        }
+
+        buffer_write(buf, c);
+    }
+
+    buffer_write(buf, 0x00);
+    return token_create(&(struct token){.type = TOKEN_TYPE_STRING, .sval = buffer_ptr(buf)});
+}
+
 struct token* read_next_token()    
 {
     struct token* token = NULL;
@@ -100,6 +121,10 @@ struct token* read_next_token()
     {
         NUMERIC_CASE:
         token= token_make_number();
+        break;
+
+        case '"':
+        token = token_make_string('"','"');
         break;
 
         case ' ': //we ignore whitespace
